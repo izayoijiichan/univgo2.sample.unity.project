bd4e4aa8d290697258774d1c2859d8fd6f9f7d1d
IzayoiJiichan <izayoi.jiichan@vtuber.email>
2022-02-27 00:00:00 +0900

    initial commit

---
+++ Assets/Demos/Scripts/RuntimeLoadBehaviour.cs
@@ -0,0 +1,28 @@
+﻿namespace UniVgoDemos
+{
+    using System.IO;
+    using UnityEngine;
+    using UniVgo2;
+
+    public class RuntimeLoadBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private string _LocalFilePath = null;
+
+        private void Start()
+        {
+            if (File.Exists(_LocalFilePath))
+            {
+                var vgoImporter = new VgoImporter();
+
+                vgoImporter.Load(_LocalFilePath);
+
+                vgoImporter.ReflectSkybox(Camera.main);
+            }
+            else
+            {
+                Debug.LogWarningFormat("File is not found. {0}", _LocalFilePath);
+            }
+        }
+    }
+}
